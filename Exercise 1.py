8ca2e33993ab239b6c7ff05e145dee5e388a417c
Musa-sabiya <musabiya112@gmail.com>
2024-08-28 11:12:08 +0100

    Add files via upload

---
+++ Exercise 1.py
@@ -0,0 +1,11 @@
+# python programming
+first_name= 'Musa'
+last_name= 'Salihu'
+full_name= 'Musa Salihu'
+country= 'Nigeria'
+city= 'Gombe'
+age= 40
+year= 2024
+is_married= True
+is_light_on= True
+first_name, last_name, country, age, year, is_married, is_true, is_light_on= 'Musa', 'Salihu', 'Nigeria', 'Gombe', 40, 2024, True, True
