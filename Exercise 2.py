ab2b8b386aaaa0e6a2820f7549232783c4823d6c
Musa-sabiya <musabiya112@gmail.com>
2024-09-07 12:34:25 +0100

    Add files via upload

---
+++ Exercise 2.py
@@ -0,0 +1,21 @@
+#python programming
+type(first_name)
+type(last_name)
+type(country)
+type(age)
+type(year)
+len(first_name)
+len(first_name) && len(last_name)
+num_one=5
+num_two=4
+addition=num_one+num_two
+substraction=num_one-num_two
+multiplication=num_one*num_two
+division=num_one/num_two
+modulus=num-two%num_one
+exponential=num_one**num_two
+floor_division=num_one//num_two
+radius= 30 meters
+area_of_circle= 3.142*radius**2
+circum_of_circle= 2*3.142.*radius
+help('keywords')
