0207f9808afa25be4291a1226630f611eacf0139
Musa sabiya <musabiya112@gmail.com>
2024-12-11 15:38:19 +0100

    Add files via upload

---
+++ Final Exercise.py
@@ -0,0 +1,43 @@
+# syntax
+lst = list()
+empty_list = list()
+
+names = ['Ahmed', 'Bello', 'Isa', 'Lawal', 'Dada', 'Bilki', 'Ado']
+list of names
+len(names)
+first_name = names[-7]
+middle_name = names[-4]
+last_name = names[-1]
+mixed_data_types = ['Musa', 40, 1.7, 'Married', 'Commercial Area Gombe']
+it_companies = [ 'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+first_company, second_company, third_company, *rest = it_com
+print('Companies:',it_companies)
+print('Number of companies:', len(it_companies))
+First_company=company[0]
+Middle_company=company[3]
+Last_company=company[-1]
+print(First_company)
+print(Middle_company)
+Print(Last_company)
+it_companies[0]='HP'
+print(it_companies)
+it_companies.append('HP')
+it_companies.insert(3,'HP')
+it_companies[3]='APPLE'
+it_companies = [ 'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+string=#;
+it_companies and string=it_companies+string
+does_exist='IBM' in it_companies
+it_companies.sort()
+reverse_company=it_companies[::-1]
+del it_companies[0:3]
+del it_companies[3:6]
+del it_companies[1]
+del it_companies[3]
+del it_companies[6]
+del it_companies[]
+Front_end=['HTML','CSS','JS','React','Redux']
+back_end=['Node','Express','MangoDB']
+Joi_ned=Front_end + back_end
+Front_end[3:4]='python','SQL'
+
